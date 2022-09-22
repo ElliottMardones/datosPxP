62598b73c8029961bf3638a66492ac0a69d0c6d3
Elliott Mardones <elliottmardones@gmail.com>
2022-09-22 14:33:26 -0300

    Update
    Update

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' max_ByCountry usando foRgotten
+#'
+#' Los datos fueron calculados usando la funcion FE de foRgotten.
+#' Los parametros son: thr = 0.5, reps = 1000
+#' @usage Ver ejemplo
+#' @examples
+#' library(datosPxP)
+#' library(foRgotten)
+#' dd <- datosPxP::datos
+#' max_By_Year <- maxByYear(dd)
+#' summary(max_By_Year)
+#' max_By_Year_FE <- foRgotten::FE(CE = max_By_Year, thr = 0.5, maxOrder = 2, reps = 1000)
+#' head(max_By_Year_FE$boot$Order_2)
+
+"max_By_Year_FE"
+
+
+
+
+#' max_By_Country_FE usando foRgotten
+#'
+#' Los datos fueron calculados usando la funcion FE de foRgotten.
+#' Los parametros son: thr = 0.05, reps = 1000
+#' @usage Ver ejemplo
+#' @examples
+#' library(datosPxP)
+#' library(foRgotten)
+#' dd <- datosPxP::datos
+#' max_By_Country <- max_ByCountry(dd)
+#' summary(max_By_Country)
+#' max_By_Country_FE <- foRgotten::FE(CE = max_By_Country, thr = 0.05, maxOrder = 2, reps = 1000)
+#' max_By_Country_FE$boot$Order_2
+"max_By_Country_FE"
